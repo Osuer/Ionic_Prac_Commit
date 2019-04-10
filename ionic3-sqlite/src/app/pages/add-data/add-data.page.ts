9407a147287f4d1eee093b27ebb268e747facf68
Osuer <47419934+Osuer@users.noreply.github.com>
2019-04-10 23:42:06 +0200

    Commit for Tomorrow
    Yeet

---
+++ ionic3-sqlite/src/app/pages/add-data/add-data.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { Toast } from '@ionic-native/toast/ngx';
+
+
+@Component({
+  selector: 'app-add-data',
+  templateUrl: './add-data.page.html',
+  styleUrls: ['./add-data.page.scss'],
+})
+export class AddDataPage implements OnInit {
+  data = { date:"", type:"", description:"", amount:0 };
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private sqlite: SQLite,
+    private toast: Toast) { }
+
+  ngOnInit() {
+  }
+
+
+  saveData() {
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO expense VALUES(NULL,?,?,?,?)',[this.data.date,this.data.type,this.data.description,this.data.amount])
+        .then(res => {
+          console.log(res);
+          this.toast.show('Data saved', '5000', 'center').subscribe(
+            toast => {
+              //this.navCtrl.popToRoot();
+            }
+          );
+        })
+        .catch(e => {
+          console.log(e);
+          this.toast.show(e, '5000', 'center').subscribe(
+            toast => {
+              console.log(toast);
+            }
+          );
+        });
+    }).catch(e => {
+      console.log(e);
+      this.toast.show(e, '5000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  }
+
+}
+
+
